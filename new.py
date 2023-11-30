6197c5c0d0a28d8fc3a356389b536c499c05ea3c
VKabil <kabilvkabil@gmail.com>
2023-11-30 19:45:15 +0530

    Create Target Array

--- new.py
+++ new.py
@@ -1,4 +1,12 @@
-a=[12,34,23,10,50]
 
-for i in range(len(a)):
-    print(a)
+#Create Target Array in the Given Order
+
+nums = [0,1,2,3,4]
+index = [0,1,2,2,1]
+
+target_index=[]
+for i in range(len(nums)):
+    target_index.insert(index[i], nums[i])
+
+print(target_index)
+
